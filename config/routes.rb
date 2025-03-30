2cfef31b53b995a38b8134dc6b2f88e9375352d0
cdwhite2 <cdwhite@protonmail.com>
2025-03-30 02:25:14 +0000

    refactored edit form and update action

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,24 @@ Rails.application.routes.draw do
 
   # Routes for the Movie resource:
 
-  # CREATE
-  post "/movies" => "movies#create", as: :movies # movies_url and movies_path
-  get "/movies/new" => "movies#new", as: :new_movie # new_movie_url and new_movie_path
+  resources :movies
 
-  # READ
-  get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show", as: :movie # movie_path() (expects an argument to populate :id segment)
+  resources :directors
 
-  # UPDATE
-  patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit", as: :edit_movie # edit_movie_path() (expects an argument to populate :id segment) 
+  # # CREATE
+  # post "/movies" => "movies#create", as: :movies # movies_url and movies_path
+  # get "/movies/new" => "movies#new", as: :new_movie # new_movie_url and new_movie_path
 
-  # DELETE
-  delete "/movies/:id" => "movies#destroy"
+  # # READ
+  # get "/movies" => "movies#index"
+  # get "/movies/:id" => "movies#show", as: :movie # movie_path() (expects an argument to populate :id segment)
+
+  # # UPDATE
+  # patch "/movies/:id" => "movies#update"
+  # get "/movies/:id/edit" => "movies#edit", as: :edit_movie # edit_movie_path() (expects an argument to populate :id segment) 
+
+  # # DELETE
+  # delete "/movies/:id" => "movies#destroy"
 
   #------------------------------
 end
